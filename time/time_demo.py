097661284f6782df22443e3b85e61a0711551f49
cag2050 <cag2050@cagdeMacBook-Pro.local>
2020-10-20 15:17:47 +0800

    add: yaml_to_python_object.py

---
+++ time/time_demo.py
@@ -0,0 +1,11 @@
+import time
+
+time_start = time.time()
+print('time_start')
+print(time_start)
+
+time_end = time.time()
+print('time_end')
+print(time_end)
+
+print('time cost:', time_end - time_start, 's')
